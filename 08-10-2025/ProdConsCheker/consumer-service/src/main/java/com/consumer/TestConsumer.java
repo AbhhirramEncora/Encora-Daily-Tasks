5d11b5ca7abc9993716d07a53d40705a89461275
Abhhirram Veedhi <veedhi.abhhirram@encora.com>
2025-10-09 11:59:20 +0530

    Add files via upload

---
+++ 08-10-2025/ProdConsCheker/consumer-service/src/main/java/com/consumer/TestConsumer.java
@@ -0,0 +1,12 @@
+package com.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class TestConsumer {
+	public static void main(String args[]) {
+		SpringApplication.run(TestConsumer.class,args);
+	}
+ 
+}
